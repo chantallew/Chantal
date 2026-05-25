f46682895dd7fd25857d62d060a0bb04c0ae1ab1
chantallew <chantallew08@gmail.com>
2026-05-25 05:31:43 +0000

    done

---
+++ Part2/ex10.py
@@ -0,0 +1,6 @@
+def say_hi(fname):
+    print("Greetings "+ fname)
+
+say_hi("Marvin!")
+say_hi("Ben!")
+
---
+++ Part2/ex11.py
@@ -0,0 +1,8 @@
+def add_two(x):
+    return (x + 2)
+
+print(add_two(4))
+results = add_two(8)
+
+print(results)
+
